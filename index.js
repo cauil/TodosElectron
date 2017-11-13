fcb58c39e0d78a99d1f1f27c3035b87fd61508cc
cauil <xiaoloujun@gmail.com>
2017-11-13 22:48:26 +0800

    add subMenu

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const url = require('url');
 
 const {app, BrowserWindow, Menu} = electron;
 
-let mainWindow;
+let mainWindow, addWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({});
@@ -19,18 +19,32 @@ app.on('ready', () => {
   Menu.setApplicationMenu(mainMenu);
 });
 
+function createAddWindow() {
+  addWindow = new BrowserWindow({});
+
+  addWindow.loadURL(url.format({
+    pathname: path.resolve(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+}
+
 const mainMenuTemplate = [
   {
     label: 'File',
     submenu: [
       {
         label: 'Add Item',
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: 'Clear Item',
       },
       {
         label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
         click() {
           app.quit();
         }
